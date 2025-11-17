d6cabc3725c7e1c29ad1ce157aba6e7fc9516e69
salagreg <greg.sala@icloud.com>
2025-11-17 23:16:12 +0100

    Concatener deux chaines de caracteres

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module qui contient une fonction qui concatène deux chaines de caracteres"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatener deux chaines de caracteres"""
+    return str1 + str2
