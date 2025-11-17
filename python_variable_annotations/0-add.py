766e512aa7d86ab3e3bd23b8ed0b8918e2d1382f
salagreg <greg.sala@icloud.com>
2025-11-17 23:15:45 +0100

    Retourner la somme sous forme de float

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module qui contient une fonction pour additionner deux nombres flottants"""
+
+
 def add(a: float, b: float) -> float:
     """Retourne la somme sous forme de float"""
     return a + b
