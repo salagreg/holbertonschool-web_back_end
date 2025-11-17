6393ff8f03feeb4387bc2a942f9215412444a66d
salagreg <greg.sala@icloud.com>
2025-11-17 23:48:44 +0100

    Retourner la partie inferieur d'un nombre flottant

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Module qui contient une fonction pour obtenir
+        la partie entière inférieure d'un nombre flottant """
+
+
+def floor(n: float) -> int:
+    """Retourne la partie entière inférieure d'un nombre flottant"""
+    return int(n // 1)
