23749ccd884c68d4d278257ff50a511b946f72a0
salagreg <greg.sala@icloud.com>
2025-11-18 15:11:27 +0100

    Retourne un tuple qui contient deux arguments

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module qui contient la fonction to_kv et qui retourne un tuple."""
+
+
+def to_kv(k: str, v: int | float) -> tuple:
+    """Retourne un tuple contenant deux arguments."""
+    return (k, v)
