6da28e2475da76a945d8dab07b6f3f1509071505
salagreg <greg.sala@icloud.com>
2025-11-18 14:27:15 +0100

    Retourner la somme d'une liste de flottants

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module qui renvoie la somme d'une liste de flottants."""
+
+
+def sum_list(input_list: float) -> float:
+    """Renvoie la somme d'une liste de flottants."""
+    total: float = 0.0
+    for num in input_list:
+        total += num
+    return total
