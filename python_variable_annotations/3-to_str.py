52aef124e7ea22ccb56f872ddfe6897a7c6b2957
salagreg <greg.sala@icloud.com>
2025-11-18 09:47:50 +0100

    Retourner un argument flottant en chaine de caracteres

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module qui prend un flottant comme argument
+        et retourne sa représentation en chaîne de caractères."""
+
+
+def to_str(n: float) -> str:
+    """Convertit un flottant en chaîne de caractères"""
+    return str(n)
