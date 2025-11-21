3374ea0cc8034f07db06e8bae65aaa95c51fa819
salagreg <greg.sala@icloud.com>
2025-11-21 15:50:40 +0100

    Mise à jour fichier

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Module qui renvoie la somme d'une liste de flottants."""
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Renvoie la somme d'une liste de flottants."""
     total: float = 0.0
     for num in input_list:
