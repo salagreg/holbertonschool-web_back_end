65bb96353915e1c79c70b6fdb74bbc131a46bc3d
salagreg <greg.sala@icloud.com>
2025-12-01 22:06:35 +0100

    Implémenter une fonction d'assistance simple

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module qui contient une fonction d'aide pour la pagination"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ Retourne un tuple de taille deux contenant"""
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
