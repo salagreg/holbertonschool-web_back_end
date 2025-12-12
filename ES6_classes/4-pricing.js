88f073d4ca499dc9845687154082fc713ed5d397
salagreg <greg.sala@icloud.com>
2025-12-12 16:27:43 +0100

    Implémenter une méthode statique

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+  get amount() {
+    return this._amount;
+  }
+  get currency() {
+    return this._currency;
+  }
+  set amount(value) {
+    this._amount = value;
+  }
+  set currency(value) {
+    this._currency = value;
+  }
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
