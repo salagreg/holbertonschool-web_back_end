0b91ea93e747756b7ecd6ac1fccfec7763a6999e
salagreg <greg.sala@icloud.com>
2025-12-12 18:11:46 +0100

    Ajout du fichier 6 dans repository

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,20 +2,19 @@ import Building from './5-building.js';
 
 export default class SkyHigh extends Building {
   constructor(sqft, floors) {
-    super(sqft, 100);
+    super(sqft);
     this._floors = floors;
-    this._sqft = sqft;
   }
+
   get floors() {
     return this._floors;
   }
-  get sqft() {
-    return this._sqft;
-  }
+
   removalWarningMessage() {
-    return 'SkyHigh buildings cannot be removed';
+    return 'SkyHigh cannot be removed';
   }
+
   evacuationWarningMessage() {
-    return `Evacuate the building immediately. There are ${this._floors} floors!`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
