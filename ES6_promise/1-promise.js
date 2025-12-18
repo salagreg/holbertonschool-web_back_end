997f3aa01a063caba72df4ebd7f5d289bb7d0126
salagreg <greg.sala@icloud.com>
2025-12-18 12:45:39 +0100

    Retourner une promesse

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,11 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-  if (success) resolve({status: 200, body: 'Success'});
-  else reject('The fake API is not working currently');
-});
+    if (success) {
+      const obj = ({status: 200, body: 'Success'})
+      resolve(obj);
+    }
+    else {
+    reject('The fake API is not working currently');
+    }
+  });
 }
