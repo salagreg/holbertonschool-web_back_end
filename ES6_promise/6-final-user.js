ad080eab2a71bfc2112c9666947520279ecab9b8
salagreg <greg.sala@icloud.com>
2025-12-19 23:35:00 +0100

    Ajout des fichiers dans repository

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    }))
+  );
 }
