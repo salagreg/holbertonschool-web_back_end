11010e491b2dfb9dce327d27f2eeef2e88aa7818
salagreg <greg.sala@icloud.com>
2026-01-08 15:08:29 +0100

    Ajout des fichiers dans repository

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
-
-process.stdin.on('data', (data) => {
+process.stdin.on('data', function(data) {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 });
 
-process.stdin.on('end', () => {
+process.stdin.on('end', function() {
   console.log('This important software is now closing');
 });
